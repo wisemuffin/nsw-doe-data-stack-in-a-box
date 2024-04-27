eda53f6a3aefbd0a54ab3e65d129fbffcc442e1b
david <davidgg777@hotmail.com>
2024-04-27 10:50:47 +1000

    added google tag manager tracking

--- reports/pages/+layout.svelte
+++ reports/pages/+layout.svelte
@@ -5,6 +5,19 @@
 	export let data;
 </script>
 
+<svelte:head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-724H1YWVH7"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-724H1YWVH7');
+  </script>
+</svelte:head>
+
+
 <EvidenceDefaultLayout {data} title="NSW DOE Data Stack in a Box">
 	<slot slot="content" />
 </EvidenceDefaultLayout>

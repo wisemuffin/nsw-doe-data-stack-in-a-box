169808e239bf51ca6905bfa07a386572af271736
david <davidgg777@hotmail.com>
2024-04-27 10:49:05 +1000

    adding staging tables for github

--- orchestration/pipeline_nsw_doe/assets/raw_dlt/assets.py
+++ orchestration/pipeline_nsw_doe/assets/raw_dlt/assets.py
@@ -40,10 +40,10 @@ class GithubDagsterDltTranslator(DagsterDltTranslator):
         items_per_page=100,
         max_items=10
     )
-    # .with_resources("issues")
+    .with_resources("issues")
     ,
     dlt_pipeline=pipeline(
-        pipeline_name="github_issues",
+        pipeline_name="github_github_reactions",
         dataset_name="raw_github", # schema
         destination="duckdb"
     ),
@@ -57,27 +57,28 @@ def github_reactions_dagster_assets(context: AssetExecutionContext, dlt: Dagster
 
 
 
-@dlt_assets(
-    dlt_source=github_repo_events(
-        # owner= "wisemuffin",
-        # name="nsw-doe-data-stack-in-a-box",
-        owner="dagster-io", 
-        name="dagster", 
-        # access_token = "", #dlt.secrets.value,
-        # items_per_page=100,
-        # max_items=10
-    )
-    # .with_resources("issues")
-    ,
-    dlt_pipeline=pipeline(
-        pipeline_name="github_issues",
-        dataset_name="raw_github", # schema
-        destination="duckdb"
-    ),
-    name="github_evt",
-    # key_prefix=["raw"], # TODO: no prefixing yet
-    group_name="raw_github",
-    dlt_dagster_translator=GithubDagsterDltTranslator()
-)
-def github_repo_events_dagster_assets(context: AssetExecutionContext, dlt: DagsterDltResource):
-    yield from dlt.run(context=context)
+# @dlt_assets(
+#     dlt_source=github_repo_events(
+#         # owner= "wisemuffin",
+#         # name="nsw-doe-data-stack-in-a-box",
+#         owner="dagster-io", 
+#         name="dagster", 
+#         # access_token = "", #dlt.secrets.value,
+#         # items_per_page=100,
+#         # max_items=10
+#     )
+#     # .with_resources("repo_events")
+#     ,
+#     dlt_pipeline=pipeline(
+#         pipeline_name="github_repo_events",
+#         dataset_name="raw_github", # schema
+#         destination="duckdb"
+#     ),
+#     name="github_evt",
+#     # key_prefix=["raw"], # TODO: no prefixing yet
+#     group_name="raw_github",
+#     dlt_dagster_translator=GithubDagsterDltTranslator()
+# )
+# def github_repo_events_dagster_assets(context: AssetExecutionContext, dlt: DagsterDltResource):
+#     yield from dlt.run(context=context)
+

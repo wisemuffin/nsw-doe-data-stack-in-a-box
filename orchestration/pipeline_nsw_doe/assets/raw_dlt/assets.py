b1d2f084442bb116234d644ea3bde9869919f0cc
david <davidgg777@hotmail.com>
2024-04-28 11:54:36 +1000

    added google analytics with dlt'

--- orchestration/pipeline_nsw_doe/assets/raw_dlt/assets.py
+++ orchestration/pipeline_nsw_doe/assets/raw_dlt/assets.py
@@ -16,6 +16,7 @@ from dagster_embedded_elt.dlt import (
 from dlt import pipeline
 from dlt.extract.resource import DltResource
 from ...dlt_sources.github import github_reactions, github_repo_events
+from ...dlt_sources.google_analytics import google_analytics
 
 
 # dlt_configuration_path = file_relative_path(__file__, "../../dlt_sources/dlt_configuration.yaml")
@@ -81,4 +82,44 @@ def github_reactions_dagster_assets(context: AssetExecutionContext, dlt: Dagster
 # )
 # def github_repo_events_dagster_assets(context: AssetExecutionContext, dlt: DagsterDltResource):
 #     yield from dlt.run(context=context)
+
+
+class GoogleAnalyticsDagsterDltTranslator(DagsterDltTranslator):
+    @public
+    def get_auto_materialize_policy(self, resource: DltResource) -> Optional[AutoMaterializePolicy]:
+        return AutoMaterializePolicy.eager().with_rules(
+            AutoMaterializeRule.materialize_on_cron("0 0 * * *")
+        )
+
+
+GA_QUERIES = [
+    {
+        "resource_name": "sample_analytics_data1",
+        "dimensions": ["browser", "city"],
+        "metrics": ["totalUsers", "transactions"],
+    },
+    {
+        "resource_name": "sample_analytics_data2",
+        "dimensions": ["browser", "city", "dateHour"],
+        "metrics": ["totalUsers"],
+    },
+]
+
+@dlt_assets(
+    dlt_source=google_analytics(
+        queries=GA_QUERIES
+    )
+    ,
+    dlt_pipeline=pipeline(
+        pipeline_name="github_github_reactions",
+        dataset_name="raw_github", # schema
+        destination="duckdb"
+    ),
+    name="google_analytics",
+    # key_prefix=["raw"], # TODO: no prefixing yet
+    group_name="raw_google_analytics",
+    dlt_dagster_translator=GoogleAnalyticsDagsterDltTranslator()
+)
+def google_analytics_dagster_assets(context: AssetExecutionContext, dlt: DagsterDltResource):
+    yield from dlt.run(context=context)
 

296111b29639841e16c18a66ab67f4f0fa3771ff
david <davidgg777@hotmail.com>
2024-05-11 09:51:17 +1000

    pytest example

--- orchestration/pipeline_nsw_doe/assets/machine_learning/assets.py
+++ orchestration/pipeline_nsw_doe/assets/machine_learning/assets.py
@@ -1,4 +1,12 @@
-from dagster import AssetIn, Field, Int, file_relative_path
+import pandas as pd
+from dagster import (
+    AssetExecutionContext,
+    AssetIn,
+    Field,
+    Int,
+    asset,
+    file_relative_path,
+)
 from dagstermill import define_dagstermill_asset
 
 metrics_by_year_school_saved_query = define_dagstermill_asset(
@@ -16,3 +24,15 @@ metrics_by_year_school_saved_query = define_dagstermill_asset(
         description="The number of clusters to find",
     ),
 )
+
+
+@asset(
+    ins={"metrics_by_year_school_saved_query": AssetIn(key_prefix=["analytics"])},
+    compute_kind="pandas",
+    group_name="machine_learning",
+)
+def example_ml_for_testing(
+    context: AssetExecutionContext, metrics_by_year_school_saved_query: pd.DataFrame
+) -> pd.DataFrame:
+    context.log.info(metrics_by_year_school_saved_query.head())
+    return metrics_by_year_school_saved_query

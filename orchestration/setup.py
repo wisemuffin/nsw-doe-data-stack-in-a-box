b5a6a5d6df725856c7f3bb523858ad3ad6eee983
david <davidgg777@hotmail.com>
2024-05-29 15:12:55 +1000

    testing dagster+

--- orchestration/setup.py
+++ orchestration/setup.py
@@ -1,16 +1,13 @@
 from setuptools import find_packages, setup
 
+with open("../requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+
 setup(
     name="cese_dia_dagster",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=[
-        "dagster",
-        "dagster-cloud",
-        "dagster-dbt",
-        "dagstermill",
-        "papermill-origami>=0.0.8",
-        "pandas",
-    ],
+    install_requires=install_requires,
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )

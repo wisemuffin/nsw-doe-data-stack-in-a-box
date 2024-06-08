716dc3aa453be0fcd8543fbd5db6055936ff38ee
david <davidgg777@hotmail.com>
2024-06-08 12:01:05 +1000

    fixing saved queries in dagster plus test

--- orchestration/pipeline_nsw_doe/assets/semantic_layer/assets.py
+++ orchestration/pipeline_nsw_doe/assets/semantic_layer/assets.py
@@ -25,36 +25,12 @@ from ..transformation import nsw_doe_dbt_assets
     ],
 )
 def metrics_by_year_saved_query(context: AssetExecutionContext):
-    # import re
-    # import sys
-    # from metricflow.cli.main import cli
-    # if __name__ == "__main__":
-    #     sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
-    #     sys.exit(cli())
-
     working_dir = nsw_doe_data_stack_in_a_box_project.project_dir
-
     command = ["dbt", "docs", "generate"]
     subprocess.check_call(command, cwd=working_dir)
 
     context.log.info(f"cwd: {Path.cwd()}")
     context.log.info(f"working_dir: {working_dir}")
-
-    tmp = tempfile.NamedTemporaryFile()
-
-    context.log.info(f"tmp: {tmp}")
-
-    # Open the file for writing.
-    with open(tmp.name, "w") as f:
-        f.write("stuff")  # where `stuff` is, y'know... stuff to write (a string)
-
-    ...
-
-    # Open the file for reading.
-    with open(tmp.name) as f:
-        for line in f:
-            context.log.info(f"line: {line}")
-            ...  # more things here
 
     with tempfile.TemporaryDirectory() as tmpdirname:
         context.log.info(f"created temporary directory: {tmpdirname}")
@@ -99,32 +75,33 @@ def metrics_by_year_saved_query(context: AssetExecutionContext):
     ],
 )
 def metrics_by_year_school_saved_query(context: AssetExecutionContext):
-    csv_location = os.path.join(
-        nsw_doe_data_stack_in_a_box_project.project_dir,
-        "exports",
-        "sq-metrics-by-year-school-saved-query.csv",
-    )
-
     working_dir = nsw_doe_data_stack_in_a_box_project.project_dir
-
-    context.log.info(f"working_dir: {working_dir}")
-    context.log.info(f"csv_location: {csv_location}")
-
     command = ["dbt", "docs", "generate"]
     subprocess.check_call(command, cwd=working_dir)
 
-    command = [
-        "mf",
-        "query",
-        "--saved-query",
-        "metrics_by_year_school_saved_query",
-        "--csv",
-        csv_location,
-    ]
-    subprocess.check_call(command, cwd=working_dir)
+    context.log.info(f"cwd: {Path.cwd()}")
+    context.log.info(f"working_dir: {working_dir}")
 
-    # TODO refactor variables
-    df = pd.read_csv(csv_location, parse_dates=["metric_time__year"])
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        context.log.info(f"created temporary directory: {tmpdirname}")
+        csv_location = os.path.join(
+            tmpdirname,
+            "sq-metrics-by-year-school-saved-query.csv",
+        )
+        context.log.info(f"csv_location: {csv_location}")
+
+        command = [
+            "mf",
+            "query",
+            "--saved-query",
+            "metrics_by_year_school_saved_query",
+            "--csv",
+            csv_location,
+        ]
+        subprocess.check_call(command, cwd=working_dir)
+
+        # TODO refactor variables
+        df = pd.read_csv(csv_location, parse_dates=["metric_time__year"])
 
     # 🚧 TODO: fixing data types manually. Dont like this but ok for demos
     # df['metric_time__year'] = pd.to_datetime(df['metric_time__year']).dt.strftime('%Y-%m-%d')

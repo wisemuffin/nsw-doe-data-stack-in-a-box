b525592c9fc5b657547d60ee138d938726368bac
david <davidgg777@hotmail.com>
2024-07-03 12:10:41 +1000

    fixing sql fluff

--- transformation/transformation_nsw_doe/models/preperation/prep__enrolment.sql
+++ transformation/transformation_nsw_doe/models/preperation/prep__enrolment.sql
@@ -1,43 +1,50 @@
 with unioned as (
     select
-        school_performance_directorate,
-        school_code,
-        school_name,
-        calendar_year,
-        enrolments_central as enrolments,
+        central.school_performance_directorate,
+        central.school_code,
+        central.school_name,
+        central.calendar_year,
+        central.enrolments_central as enrolments,
         'central' as school_type,
-    from {{ ref('stg__nsw_doe_datansw__enrolments_central') }}
+    from {{ ref('stg__nsw_doe_datansw__enrolments_central') }} as central
     union all
     select
-        school_performance_directorate,
-        school_code,
-        school_name,
-        calendar_year,
-        enrolments_primary as enrolments,
+        prim.school_performance_directorate,
+        prim.school_code,
+        prim.school_name,
+        prim.calendar_year,
+        prim.enrolments_primary as enrolments,
         'primary' as school_type,
-    from {{ ref('stg__nsw_doe_datansw__enrolments_primary') }}
+    from {{ ref('stg__nsw_doe_datansw__enrolments_primary') }} as prim
     union all
     select
-        school_performance_directorate,
-        school_code,
-        school_name,
-        calendar_year,
-        enrolments_secondary as enrolments,
+        secondary.school_performance_directorate,
+        secondary.school_code,
+        secondary.school_name,
+        secondary.calendar_year,
+        secondary.enrolments_secondary as enrolments,
         'secondary' as school_type,
-    from {{ ref('stg__nsw_doe_datansw__enrolments_secondary') }}
+    from {{ ref('stg__nsw_doe_datansw__enrolments_secondary') }} as secondary
     union all
     select
-        school_performance_directorate,
-        school_code,
-        school_name,
-        calendar_year,
-        enrolments_ssp as enrolments,
+        ssp.school_performance_directorate,
+        ssp.school_code,
+        ssp.school_name,
+        ssp.calendar_year,
+        ssp.enrolments_ssp as enrolments,
         'ssp' as school_type,
-    from {{ ref('stg__nsw_doe_datansw__enrolments_ssp') }}
-)
-, final as (
+    from {{ ref('stg__nsw_doe_datansw__enrolments_ssp') }} as ssp
+),
+
+final as (
+    select
+        unioned.school_performance_directorate,
+        unioned.school_code,
+        unioned.school_name,
+        unioned.calendar_year,
+        unioned.enrolments,
     from unioned
-    where school_code != '-' -- data quality issue
+    where unioned.school_code != '-' -- data quality issue
 )
 
 {{ dbt_audit(

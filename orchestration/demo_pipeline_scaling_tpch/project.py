9a4f5567e11d46fba39f54532eeb09fde649e162
david <davidgg777@hotmail.com>
2024-10-19 12:31:55 +1100

    hot fix deployment to dagster plus

--- orchestration/demo_pipeline_scaling_tpch/project.py
+++ orchestration/demo_pipeline_scaling_tpch/project.py
@@ -13,12 +13,6 @@ project_dir = (
 
 dbt = DbtCliResource(project_dir=str(project_dir))
 
-if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD") or not os.path.exists(
-    os.path.join(project_dir, "target")
-):
-    dbt_parse_invocation = dbt.cli(["parse"]).wait()
-
-
 tpch_project = DbtProject(
     project_dir=project_dir,
     packaged_project_dir=Path(__file__).joinpath("..", "dbt-project-temp").resolve(),
--- orchestration/pipeline_nsw_doe/project.py
+++ orchestration/pipeline_nsw_doe/project.py
@@ -15,12 +15,6 @@ project_dir = (
 
 dbt = DbtCliResource(project_dir=str(project_dir))
 
-if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD") or not os.path.exists(
-    os.path.join(project_dir, "target")
-):
-    dbt_parse_invocation = dbt.cli(["parse"]).wait()
-
-
 nsw_doe_data_stack_in_a_box_project = DbtProject(
     project_dir=project_dir,
     packaged_project_dir=Path(__file__).joinpath("..", "dbt-project-temp").resolve(),

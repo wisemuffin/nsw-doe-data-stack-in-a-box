24f29b38d78d0286ea33159cd2f3bb73c78f4ffa
david <davidgg777@hotmail.com>
2024-11-07 17:22:59 +1100

    adding data quality examples

--- transformation/transformation_nsw_doe/data_tests/assert_example_master_dataset.sql
+++ transformation/transformation_nsw_doe/data_tests/assert_example_master_dataset.sql
@@ -1,1 +1,2 @@
-select *, from {{ ref('stg__nsw_doe_datansw__master_dataset') }} where School_code < 9000
+{# if this bring back results then its a fail #}
+select *, from {{ ref('stg__nsw_doe_datansw__master_dataset') }} where School_code > 9000
